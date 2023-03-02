b39778ba9c5188de0f4813f6561842e61aea1b06
Vinayak Vispute <93467074+VinayakVispute@users.noreply.github.com>
2023-03-03 04:04:37 +0530

    Updated

--- 01-task-manager/controllers/task.js
+++ 01-task-manager/controllers/task.js
@@ -1,16 +1,38 @@
-const Task = require('../models/Task')
+const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-  return res.send("All Items from file");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
-const task = await Task.create(req.body);
-  return res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    return res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
-const getTask = (req, res) => {
-  return res.send("Get a single task");
+
+const getTask = async (req, res) => {
+  try {
+    const tas k = await Task.findOne({_id : taskID})
+    const {id:taskID} = req.params
+    if(!task){
+      return res.status(404).json({msg : `No Task found with this ID : {taskID}` })
+    }else{
+    return res.status(200).json({ task });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error });
+ 
+  }
 };
+
 const updateTask = (req, res) => {
   return res.send("Update the task");
 };
@@ -23,5 +45,5 @@ module.exports = {
   createTask,
   getTask,
   updateTask,
-  deleteTask
+  deleteTask,
 };
--- 01-task-manager/models/Task.js
+++ 01-task-manager/models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type:String,
+    required:[true,'Please Provide Name....!Required'],
+    trim:true,
+    maxlength:[20,'Cant Be More than 20 Words']
+  },
+  completed: {
+    type:Boolean,
+    default : false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
