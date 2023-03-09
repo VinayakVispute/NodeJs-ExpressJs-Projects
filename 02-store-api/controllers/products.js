d06dacfd61bacbe0bd783e9dead43fffd90f2a2a
Vinayak Vispute <93467074+VinayakVispute@users.noreply.github.com>
2023-03-09 23:38:35 +0530

    updated

--- 02-store-api/controllers/products.js
+++ 02-store-api/controllers/products.js
@@ -1,20 +1,29 @@
-const Product = require('../models/product')
+const Product = require("../models/product");
 
-const getAllProductsStatic = async(req,res) =>{
-    const products = await Product.find({
-        name:'vase table',
-    })
-    return res.status(200).send({products,nbHits:products.length})
-}
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({
+    name: "vase table",
+  });
+  return res.status(200).send({ products, nbHits: products.length });
+};
 
-const getAllProducts = async(req,res) =>{
-    const products = await Product.find(req.query)
-    return res.status(200).send({products,nbHits:products.length})
+const getAllProducts = async (req, res) => {
+  // const products = await Product.find(req.query)
+  const { featured, company } = req.query;
+  const queryObject = {};
 
-}
-
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company){
+    queryObject.company = company;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  return res.status(200).send({ products, nbHits: products.length });
+};
 
 module.exports = {
-    getAllProductsStatic,
-    getAllProducts,
-}
+  getAllProductsStatic,
+  getAllProducts,
+};
