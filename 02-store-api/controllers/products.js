b3756af3757a8d54f9676548d22d2ff086beeaff
Vinayak Vispute <93467074+VinayakVispute@users.noreply.github.com>
2023-03-10 03:10:07 +0530

    Updated

--- 02-store-api/controllers/products.js
+++ 02-store-api/controllers/products.js
@@ -2,13 +2,13 @@ const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
   const search = "AB";
-  const products = await Product.find({}).sort('-name');
+  const products = await Product.find({}).sort("-name");
   return res.status(200).send({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   // const products = await Product.find(req.query)
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -20,8 +20,20 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+//   console.log(queryObject);
+
+  let results =  Product.find(queryObject);
+
+
+  if (sort) {
+    // console.log(sort);
+    const sortList = sort.split(',').join(' ');
+    console.log(sortList);
+    results = results.sort(sortList)
+  }else {
+    results = results.sort('createdAt')
+  }
+  const products = await results;
   return res.status(200).send({ products, nbHits: products.length });
 };
 
