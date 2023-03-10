38ac63f6e2521c72ab38b1d3ebc24c38dce36af2
Vinayak Vispute <93467074+VinayakVispute@users.noreply.github.com>
2023-03-11 00:02:49 +0530

    Update products.js

--- 02-store-api/controllers/products.js
+++ 02-store-api/controllers/products.js
@@ -12,7 +12,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   // const products = await Product.find(req.query)
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -23,6 +23,31 @@ const getAllProducts = async (req, res) => {
   }
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
+  }
+  if (numericFilters) {
+    console.log(numericFilters);
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = {
+          [operator]: Number(value),
+        };
+      }
+    });
+    console.log(queryObject);
   }
 
   let results = Product.find(queryObject);
@@ -44,9 +69,9 @@ const getAllProducts = async (req, res) => {
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
-  results = results.skip(skip).limit(limit)
+  results = results.skip(skip).limit(limit);
   const products = await results;
-  return res.status(200).send({ products, nbHits: products.length });
+  return res.status(200).send({ nbHits: products.length, products });
 };
 
 module.exports = {
