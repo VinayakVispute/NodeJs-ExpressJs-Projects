bf275e7c19c0189fb33ed03208974c86ae9853a4
Vinayak Vispute <93467074+VinayakVispute@users.noreply.github.com>
2023-03-11 04:05:55 +0530

    Updated

--- 03-JWT-Basics/routes/main.js
+++ 03-JWT-Basics/routes/main.js
@@ -1,10 +1,5 @@
+const express = require('express');
 
+const route = express.Router();
 
-const login = async (req,res) => {
-    res.send("Fake Login/Register/SignUp Route")
-}
-
-const dashboard = async (req,res) => {
-    const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg: `Hello Vinayak Vispute`,secret : `Here is your authorized data, your luck number is ${luckyNumber}`})
-}
+const {login, dashboard} = require('../controllers/main')
